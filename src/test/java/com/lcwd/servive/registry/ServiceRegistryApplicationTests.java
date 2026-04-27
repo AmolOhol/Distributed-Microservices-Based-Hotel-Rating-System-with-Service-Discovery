eb518db2edaa983b25c02b2bcbafc0d3dd69a6fb
AmolOhol <127683693+AmolOhol@users.noreply.github.com>
2026-04-27 15:20:33 +0530

    first commit

---
+++ src/test/java/com/lcwd/servive/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.servive.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
