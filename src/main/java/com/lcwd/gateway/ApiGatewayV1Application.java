14d6622b31414d2fc442d424a687c4945344bb5e
AmolOhol <127683693+AmolOhol@users.noreply.github.com>
2026-04-27 14:58:38 +0530

    first commit

---
+++ src/main/java/com/lcwd/gateway/ApiGatewayV1Application.java
@@ -0,0 +1,15 @@
+package com.lcwd.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayV1Application.class, args);
+	}
+
+}
